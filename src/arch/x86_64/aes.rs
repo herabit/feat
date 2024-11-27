5d86d9d149621a6362c3d9bcf88f06d2b1af0e59
Hera Chamorro <hera@hera.wtf>
2024-11-26 16:54:47 -0800

    Change bitwise casts
    Change various bitwise casts to use the wrappers around bytemuck

--- src/arch/x86_64/aes.rs
+++ src/arch/x86_64/aes.rs
@@ -3,8 +3,8 @@ use super::intrin::{
     _mm_aesimc_si128, _mm_aeskeygenassist_si128,
 };
 use super::Aes;
+use crate::muck::cast;
 use crate::ty::u8x16;
-use bytemuck::must_cast;
 
 impl Aes {
     #[inline(always)]
@@ -12,7 +12,7 @@ impl Aes {
     #[doc(alias = "_mm_aesdec_si128")]
     pub fn decrypt_128(self, a: u8x16, round_key: u8x16) -> u8x16 {
         // SAFETY: We know that AES is enabled.
-        must_cast(unsafe { _mm_aesdec_si128(must_cast(a), must_cast(round_key)) })
+        cast(unsafe { _mm_aesdec_si128(cast(a), cast(round_key)) })
     }
 
     #[inline(always)]
@@ -20,7 +20,7 @@ impl Aes {
     #[doc(alias = "_mm_aesdeclast_si128")]
     pub fn decrypt_last_128(self, a: u8x16, round_key: u8x16) -> u8x16 {
         // SAFETY: We know that AES is enabled.
-        must_cast(unsafe { _mm_aesdeclast_si128(must_cast(a), must_cast(round_key)) })
+        cast(unsafe { _mm_aesdeclast_si128(cast(a), cast(round_key)) })
     }
 
     #[inline(always)]
@@ -28,7 +28,7 @@ impl Aes {
     #[doc(alias = "_mm_aesenc_si128")]
     pub fn encrypt_128(self, a: u8x16, round_key: u8x16) -> u8x16 {
         // SAFETY: We know that AES is enabled.
-        must_cast(unsafe { _mm_aesenc_si128(must_cast(a), must_cast(round_key)) })
+        cast(unsafe { _mm_aesenc_si128(cast(a), cast(round_key)) })
     }
 
     #[inline(always)]
@@ -36,7 +36,7 @@ impl Aes {
     #[doc(alias = "_mm_aesenclast_si128")]
     pub fn encrypt_last_128(self, a: u8x16, round_key: u8x16) -> u8x16 {
         // SAFETY: We know that AES is enabled.
-        must_cast(unsafe { _mm_aesenclast_si128(must_cast(a), must_cast(round_key)) })
+        cast(unsafe { _mm_aesenclast_si128(cast(a), cast(round_key)) })
     }
 
     #[inline(always)]
@@ -44,7 +44,7 @@ impl Aes {
     #[doc(alias = "_mm_aesimc_si128")]
     pub fn inv_mix_column_128(self, a: u8x16) -> u8x16 {
         // SAFETY: We know that AES is enabled.
-        must_cast(unsafe { _mm_aesimc_si128(must_cast(a)) })
+        cast(unsafe { _mm_aesimc_si128(cast(a)) })
     }
 
     #[inline(always)]
@@ -52,6 +52,6 @@ impl Aes {
     #[doc(alias = "_mm_aeskeygenassist_si128")]
     pub fn keygen_assist_128<const IMM8: i32>(self, a: u8x16) -> u8x16 {
         // SAFETY: We know that AES is enabled.
-        must_cast(unsafe { _mm_aeskeygenassist_si128::<IMM8>(must_cast(a)) })
+        cast(unsafe { _mm_aeskeygenassist_si128::<IMM8>(cast(a)) })
     }
 }
--- src/arch/x86_64/sha.rs
+++ src/arch/x86_64/sha.rs
@@ -1,9 +1,8 @@
-use bytemuck::must_cast;
-
 use super::intrin::{
     _mm_sha1msg1_epu32, _mm_sha1msg2_epu32, _mm_sha1nexte_epu32, _mm_sha1rnds4_epu32,
     _mm_sha256msg1_epu32, _mm_sha256msg2_epu32, _mm_sha256rnds2_epu32,
 };
+use crate::muck::cast;
 use crate::ty::u32x4;
 
 use super::Sha;
@@ -13,48 +12,48 @@ impl Sha {
     #[must_use]
     #[doc(alias = "_mm_sha1msg1_epu32")]
     pub fn sha1_msg1(self, a: u32x4, b: u32x4) -> u32x4 {
-        must_cast(unsafe { _mm_sha1msg1_epu32(must_cast(a), must_cast(b)) })
+        cast(unsafe { _mm_sha1msg1_epu32(cast(a), cast(b)) })
     }
 
     #[inline(always)]
     #[must_use]
     #[doc(alias = "_mm_sha1msg2_epu32")]
     pub fn sha1_msg2(self, a: u32x4, b: u32x4) -> u32x4 {
-        must_cast(unsafe { _mm_sha1msg2_epu32(must_cast(a), must_cast(b)) })
+        cast(unsafe { _mm_sha1msg2_epu32(cast(a), cast(b)) })
     }
 
     #[inline(always)]
     #[must_use]
     #[doc(alias = "_mm_sha1nexte_epu32")]
     pub fn sha1_next_e(self, a: u32x4, b: u32x4) -> u32x4 {
-        must_cast(unsafe { _mm_sha1nexte_epu32(must_cast(a), must_cast(b)) })
+        cast(unsafe { _mm_sha1nexte_epu32(cast(a), cast(b)) })
     }
 
     #[inline(always)]
     #[must_use]
     #[doc(alias = "_mm_sha1rnds4_epu32")]
     pub fn sha1_rounds4<const FUNC: i32>(self, a: u32x4, b: u32x4) -> u32x4 {
-        must_cast(unsafe { _mm_sha1rnds4_epu32::<FUNC>(must_cast(a), must_cast(b)) })
+        cast(unsafe { _mm_sha1rnds4_epu32::<FUNC>(cast(a), cast(b)) })
     }
 
     #[inline(always)]
     #[must_use]
     #[doc(alias = "_mm_sha256msg1_epu32")]
     pub fn sha256_msg1(self, a: u32x4, b: u32x4) -> u32x4 {
-        must_cast(unsafe { _mm_sha256msg1_epu32(must_cast(a), must_cast(b)) })
+        cast(unsafe { _mm_sha256msg1_epu32(cast(a), cast(b)) })
     }
 
     #[inline(always)]
     #[must_use]
     #[doc(alias = "_mm_sha256msg2_epu32")]
     pub fn sha256_msg2(self, a: u32x4, b: u32x4) -> u32x4 {
-        must_cast(unsafe { _mm_sha256msg2_epu32(must_cast(a), must_cast(b)) })
+        cast(unsafe { _mm_sha256msg2_epu32(cast(a), cast(b)) })
     }
 
     #[inline(always)]
     #[must_use]
     #[doc(alias = "_mm_sha256rnds2_epu32")]
     pub fn sha256_rounds2(self, a: u32x4, b: u32x4, k: u32x4) -> u32x4 {
-        must_cast(unsafe { _mm_sha256rnds2_epu32(must_cast(a), must_cast(b), must_cast(k)) })
+        cast(unsafe { _mm_sha256rnds2_epu32(cast(a), cast(b), cast(k)) })
     }
 }
